b67ba6edc5f0cde63dba84ec19c760a29fa1284c
SorenDeveloppement <soren.evrard.lorrain@gmail.com>
2024-04-05 22:20:09 +0200

    Adding Navigation Menu

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ foreach (parse_ini_file('.env') as $key => $value) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Computer Manager</title>
 </head>
 <body>
