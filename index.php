ae09f91ffbe23c48582c7937168d7632f8e40427
SorenDeveloppement <soren.evrard.lorrain@gmail.com>
2024-04-05 21:02:52 +0200

    Applying conventions

--- index.php
+++ index.php
@@ -6,10 +6,23 @@ foreach (parse_ini_file('.env') as $key => $value) {
 ?>
 
 <html>
-<h1>
-    <?php
-    include 'backend\database\Migration.php';
-    echo createMigration();
-    ?>
-<h1>
+</html>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Computer Manager</title>
+</head>
+<body>
+
+    <h1>
+        <?php
+        include 'backend\database\Migration.php';
+        echo createMigration();
+        ?>
+    <h1>
+    
+</body>
 </html>
