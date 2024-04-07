b6f337406a3ce38ed618158195f42e32345e05c8
HulkHogan6262 <hulkhogan6262@gmail.com>
2024-04-07 21:41:52 +0200

    modifying database template for new version of ComputerManager

--- migrations/database.sql
+++ migrations/database.sql
@@ -16,6 +16,7 @@
 
 -- Listage de la structure de la table computer-manager. computer-inventory
 CREATE TABLE IF NOT EXISTS `computer-inventory` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `uuid` varchar(64) DEFAULT NULL,
   `type` varchar(50) DEFAULT NULL,
   `brand` varchar(64) DEFAULT NULL,
@@ -23,37 +24,41 @@ CREATE TABLE IF NOT EXISTS `computer-inventory` (
   `serial-number` varchar(64) DEFAULT NULL,
   `mac-address` varchar(64) DEFAULT NULL,
   `entry-date` varchar(64) DEFAULT NULL,
-  `state` varchar(64) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+  `leave-date` varchar(64) DEFAULT NULL,
+  `state` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- Listage des données de la table computer-manager.computer-inventory : ~1 rows (environ)
 DELETE FROM `computer-inventory`;
-INSERT INTO `computer-inventory` (`uuid`, `type`, `brand`, `model`, `serial-number`, `mac-address`, `entry-date`, `state`) VALUES
-	('b4fd8a8e-1932-457a-b835-e1df77028f73', '1', 'MSI', 'G41M-P25', '1234', '8C:89:A5:5A:C9:61', '1712415612', '1');
+INSERT INTO `computer-inventory` (`id`, `uuid`, `type`, `brand`, `model`, `serial-number`, `mac-address`, `entry-date`, `leave-date`, `state`) VALUES
+	(1, 'b4fd8a8e-1932-457a-b835-e1df77028f73', '1', 'MSI', 'G41M-P25', '1234', '8C:89:A5:5A:C9:61', '1712415612', '1712518301', '1');
 
 -- Listage de la structure de la table computer-manager. computer-type
 CREATE TABLE IF NOT EXISTS `computer-type` (
-  `id` varchar(50) DEFAULT NULL,
-  `visual-name` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `visual-name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- Listage des données de la table computer-manager.computer-type : ~2 rows (environ)
 DELETE FROM `computer-type`;
 INSERT INTO `computer-type` (`id`, `visual-name`) VALUES
-	('1', 'Ordinateur Portable'),
-	('2', 'Ordinateur de Bureau');
+	(1, 'Ordinateur Portable'),
+	(2, 'Ordinateur de Bureau');
 
 -- Listage de la structure de la table computer-manager. state
 CREATE TABLE IF NOT EXISTS `state` (
-  `id` varchar(50) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `visual-name` varchar(50) DEFAULT NULL,
-  `deleted` varchar(50) DEFAULT 'FALSE'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+  `deleted` varchar(50) DEFAULT 'FALSE',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Listage des données de la table computer-manager.state : ~0 rows (environ)
+-- Listage des données de la table computer-manager.state : ~1 rows (environ)
 DELETE FROM `state`;
 INSERT INTO `state` (`id`, `visual-name`, `deleted`) VALUES
-	('1', 'Nettoyage', 'FALSE');
+	(1, 'Nettoyage', 'FALSE');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
