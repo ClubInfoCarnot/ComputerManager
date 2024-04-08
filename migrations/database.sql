ca79cd4865975113ba25557fa57e80b6be6032c3
HulkHogan6262 <hulkhogan6262@gmail.com>
2024-04-08 18:39:32 +0200

    updating sql template

--- migrations/database.sql
+++ migrations/database.sql
@@ -15,6 +15,7 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 -- Listage de la structure de la table computer-manager. computer-inventory
+DROP TABLE IF EXISTS `computer-inventory`;
 CREATE TABLE IF NOT EXISTS `computer-inventory` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `type` varchar(50) DEFAULT NULL,
@@ -22,18 +23,20 @@ CREATE TABLE IF NOT EXISTS `computer-inventory` (
   `model` varchar(64) DEFAULT NULL,
   `serial-number` varchar(64) DEFAULT NULL,
   `mac-address` varchar(64) DEFAULT NULL,
-  `entry-date` varchar(64) DEFAULT NULL,
+  `entry-date` varchar(50) DEFAULT NULL,
   `leave-date` varchar(64) DEFAULT NULL,
-  `state` varchar(64) DEFAULT NULL,
+  `state` int(11) DEFAULT NULL,
+  `recipient` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- Listage des données de la table computer-manager.computer-inventory : ~1 rows (environ)
 DELETE FROM `computer-inventory`;
-INSERT INTO `computer-inventory` (`id`, `type`, `brand`, `model`, `serial-number`, `mac-address`, `entry-date`, `leave-date`, `state`) VALUES
-	(1, '1', 'MSI', 'G41M-P25', '1234', '8C:89:A5:5A:C9:61', '1712415612', '1712518301', '2');
+INSERT INTO `computer-inventory` (`id`, `type`, `brand`, `model`, `serial-number`, `mac-address`, `entry-date`, `leave-date`, `state`, `recipient`) VALUES
+	(1, '1', 'MSI', 'G41M-P25', '1234', '8C:89:A5:5A:C9:61', '1712415612', '1712518301', 1, 1);
 
 -- Listage de la structure de la table computer-manager. computer-type
+DROP TABLE IF EXISTS `computer-type`;
 CREATE TABLE IF NOT EXISTS `computer-type` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `visual-name` varchar(50) DEFAULT NULL,
@@ -46,14 +49,28 @@ INSERT INTO `computer-type` (`id`, `visual-name`) VALUES
 	(1, 'Ordinateur Portable'),
 	(2, 'Ordinateur de Bureau');
 
+-- Listage de la structure de la table computer-manager. recipient
+DROP TABLE IF EXISTS `recipient`;
+CREATE TABLE IF NOT EXISTS `recipient` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) NOT NULL DEFAULT 'Aucun Nom',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- Listage des données de la table computer-manager.recipient : ~1 rows (environ)
+DELETE FROM `recipient`;
+INSERT INTO `recipient` (`id`, `name`) VALUES
+	(1, 'Ecole de Pernes');
+
 -- Listage de la structure de la table computer-manager. state
+DROP TABLE IF EXISTS `state`;
 CREATE TABLE IF NOT EXISTS `state` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `visual-name` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Listage des données de la table computer-manager.state : ~1 rows (environ)
+-- Listage des données de la table computer-manager.state : ~2 rows (environ)
 DELETE FROM `state`;
 INSERT INTO `state` (`id`, `visual-name`) VALUES
 	(1, 'Nettoyage'),
